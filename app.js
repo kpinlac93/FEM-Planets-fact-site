62475f9fdb8eafd0f32b378c33739d655bd430cf
Kevin Pinlac <101902788+kpinlac93@users.noreply.github.com>
2022-09-27 19:35:01 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,132 @@
-/* References */
-const figureOverview = document.querySelector(".overview");
-const figureInternal = document.querySelector(".internal");
-const figureGeology = document.querySelector(".geology");
-const article01 = document.querySelector(".article-01");
-const article02 = document.querySelector(".article-02");
-const article03 = document.querySelector(".article-03");
-const nav01 = document.querySelector(".nav-01");
-const nav02 = document.querySelector(".nav-02");
-const nav03 = document.querySelector(".nav-03");
+fetch("./data.json")
+.then(response => {
+   return response.json();
+})
+.then(data => {
+    console.log(data[0].name);
+    console.log(data[0].overview.content);
+});
+
+
+// References
+const img = document.querySelector(".main-content__img");
+const imgGeology = document.querySelector(".main-content__img-geology");
+const imgGeologyPlanet = document.querySelector(".main-content__img-geology-planet");
+const imgGeologyPop = document.querySelector(".main-content__img-geology-pop");
+const header = document.querySelector(".main-content__header");
+const content = document.querySelector(".main-content__articles-desc");
+const articleNav = document.querySelectorAll(".main-content__nav-item");
+const tempStats = document.querySelectorAll(".stats__card-stat")
 
 
 
 
-/* Functions */
-function removeActive() {
-    figureOverview.classList.add("hidden");
-    figureInternal.classList.add("hidden");
-    figureGeology.classList.add("hidden");
-    article01.classList.add("hidden");
-    article02.classList.add("hidden");
-    article03.classList.add("hidden");
-    nav01.classList.remove("active");
-    nav02.classList.remove("active");
-    nav03.classList.remove("active");
-    nav01.classList.remove("inactive");
-    nav02.classList.remove("inactive");
-    nav03.classList.remove("inactive");
+
+// Mobile-view Menu
+document.querySelector(".navigation__logo").addEventListener("click", openMenu)
+function openMenu() {
+    if (document.querySelector(".navigation__links").style.display == "flex") {
+        document.querySelector(".navigation__links").style.display = "none";
+    } else {
+        document.querySelector(".navigation__links").style.display = "flex";
+    }
 }
-function toggleArticle01() {
-    removeActive()
-    figureOverview.classList.remove("hidden");
-    article01.classList.remove("hidden");
-    nav01.classList.add("active");
-    nav02.classList.add("inactive");
-    nav03.classList.add("inactive");
-}
-function toggleArticle02() {
-    removeActive()
-    figureInternal.classList.remove("hidden");
-    article02.classList.remove("hidden");
-    nav01.classList.add("inactive");
-    nav02.classList.add("active");
-    nav03.classList.add("inactive");
-}
-function toggleArticle03() {
-    removeActive()
-    figureOverview.classList.remove("hidden");
-    figureGeology.classList.remove("hidden");
-    article03.classList.remove("hidden");
-    nav01.classList.add("inactive");
-    nav02.classList.add("inactive");
-    nav03.classList.add("active");
+function normalizeMenu() {
+    document.querySelector(".navigation__links").style.display = "flex";
 }
 
 
-nav01.addEventListener("click", toggleArticle01);
-nav02.addEventListener("click", toggleArticle02);
-nav03.addEventListener("click", toggleArticle03);
+var currentIndex = null;
+function handlePlanetData(planetIndex) {
+    clearActiveState()
+    addActiveState(0)
+    normalizeMenu()
+    fetch("./data.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => {
+        document.querySelector(".navigation__links").style.display = "flex";
+        currentIndex = planetIndex;
+        header.innerHTML = data[planetIndex].name;
+        content.innerHTML = data[planetIndex].overview.content;
+        img.src = data[planetIndex].images.planet;
+        imgGeology.classList.add("hidden");
+        img.classList.remove("hidden");
+
+        tempStats[0].innerHTML = data[planetIndex].rotation;
+        tempStats[1].innerHTML = data[planetIndex].revolution;
+        tempStats[2].innerHTML = data[planetIndex].radius;
+        tempStats[3].innerHTML = data[planetIndex].temperature;
+    });
+}
+
+function clearActiveState() {
+    for (let i = 0; i < articleNav.length; i++) {
+        articleNav[i].classList.remove("main-content__nav-item--active")
+    }
+}
+function addActiveState(num) {
+    articleNav[num].classList.add("main-content__nav-item--active");
+}
+
+
+function change2Overview() {
+    if (currentIndex == null) {
+        return;
+    }
+    clearActiveState()
+    addActiveState(0)
+    fetch("./data.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => {
+        content.innerHTML = data[currentIndex].overview.content;
+        img.src = data[currentIndex].images.planet;
+        imgGeology.classList.add("hidden");
+        img.classList.remove("hidden");
+        document.querySelector(".navigation__links").style.display = "flex";
+    });
+}
+function change2Structure() {
+    if (currentIndex == null) {
+        return;
+    }
+    clearActiveState()
+    addActiveState(1)
+    fetch("./data.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => {
+        content.innerHTML = data[currentIndex].structure.content;
+        img.src = data[currentIndex].images.internal;
+        imgGeology.classList.add("hidden");
+        img.classList.remove("hidden");
+    });
+}
+
+
+
+
+
+function change2Geology() {
+    if (currentIndex == null) {
+        return;
+    }
+    clearActiveState()
+    addActiveState(2)
+    fetch("./data.json")
+    .then(response => {
+       return response.json();
+    })
+    .then(data => {
+        content.innerHTML = data[currentIndex].geology.content;
+        img.src = data[currentIndex].images.internal;
+        imgGeology.classList.remove("hidden");
+        img.classList.add("hidden");
+        imgGeologyPlanet.src = data[currentIndex].images.planet;
+        imgGeologyPop.src = data[currentIndex].images.geology;
+    });
+}
